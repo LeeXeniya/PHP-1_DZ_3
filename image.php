3056147857ac13e883175f0fadc380d38eb17a75
LeeXeniya <leexenya@gmail.com>
2021-03-11 20:49:34 +0300

    Update image.php

--- image.php
+++ image.php
@@ -1,12 +1,7 @@
 <?php
 
-$images = [
-  1 => 'cat.jpg',
-  2 => 'dog.jpg',
-  3 => 'tiger.jpg',
-];
+$images = include __DIR__ . '/send.php';
 
 if(isset($_GET['id'])): ?>
-<img src="<?php echo $images[$_GET['id']];?>">
+<img src="<?php echo $images[$_GET['id']]; ?>">
 <?php endif;
-return $images;
