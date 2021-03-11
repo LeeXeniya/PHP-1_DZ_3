27df6b0b4af10ff1d4289ab457ffd4bc130e6daa
LeeXeniya <leexenya@gmail.com>
2021-03-11 20:50:08 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 $images = include __DIR__ . '/send.php';
 
 foreach ($images as $id => $value) { ?>
-    <a href="/image.php?id=<?php echo $id ?>"><img src="<?php echo $value ?>"></a>
+    <a href="/image.php?id=<?php echo $id; ?>"><img src="<?php echo $value; ?>"></a>
 
 <?php }
 ?>
